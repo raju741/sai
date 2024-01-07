25e3255924aca5a53036977ce71c7afb174cd3a1
raju741 <balubalraj7412gmail.com>
2024-01-07 09:29:39 +0530

    any message

---
+++ src/under_sample/Demo_class.java
@@ -0,0 +1,23 @@
+package under_sample;
+
+public class Demo_class {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		Demo_class bb = new Demo_class();
+		bb.mat();
+		
+	}
+	
+	
+	
+	
+		public void mat() {
+			System.out.println("bb");
+		}
+
+	}
+
+
